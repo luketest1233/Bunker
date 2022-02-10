fee5ac338a2a1ac0663b3081f5f1d793dc85dd00
luketest1233 <78221850+luketest1233@users.noreply.github.com>
2022-02-10 13:05:46 -0500

    Update config.js

--- apps/hacker-news-tile/config.js
+++ apps/hacker-news-tile/config.js
@@ -1,16 +1,18 @@
 window.apps["hacker-news-tile"] = {};
 window.apps["hacker-news-tile"]["tile"] = `<div class="box_widget">
 	<div>
-		<h3>Hacker News</h3>	
+		<h3>Bookmarklets</h3>
+		<br>
+		<p> bookmarklets are little peices of code that u can execute. to install one, hover over the links and drag the link to your bookmarks bar. </p>
 	</div>
 	<div>
 		<ul class="list">
 		<li class="news-item"><a id="news0">Loading...</a></li>
-			<li class="news-item"><a id="news1">Loading...</a></li>
-			<li class="news-item"><a id="news2">Loading...</a></li>
-			<li class="news-item"><a id="news3">Loading...</a></li>
-			<li class="news-item"><a id="news4">Loading...</a></li>
+			<li><a href="javascript:window.location.replace('https://google.com');">Take page out of history</a></li>
+			<li ><a href="javascript:document.querySelectorAll('*').forEach(e=>e.setAttribute('style','background-color:#222;background-image:none;color:#'+(/^A|BU/.test(e.tagName)?'36c;text-decoration:underline;':'eee;')+e.getAttribute('style')))">dark mode</a></li>
+			<li ><a id="news3">Loading...</a></li>
+			<li ><a id="news4">Loading...</a></li>
 		</ul>
 	</div>
-	<script src="apps/hacker-news-tile/hckr-nws-snppts.js"></script>
+
 </div>`;
